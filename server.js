2d396a385655c3eae0f198496f16b012fc789042
José Luis Valdiviezo Peña <58832608+joseluis-max@users.noreply.github.com>
2022-03-08 17:21:13 +0000

    fix typo

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ app.get("/", (req, res) => {
 	res.send("hello world");
 })
 
-app.listen(5000, 'locahost', () => {
+app.listen(5000, 'localhost', () => {
 	console.log("Running in locahost:5000");
 });
